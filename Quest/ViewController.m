d8d781a68369de1e435a1f9720cff202d38ad32f
Georgel Vintila <gvintila@Georgels-Mac-mini.local>
2015-04-27 12:06:40 +0300

    First Commit

---
+++ Quest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Quest
+//
+//  Created by Georgel Vintila on 24/04/15.
+//  Copyright (c) 2015 Stefanini. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Parse/Parse.h>
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
